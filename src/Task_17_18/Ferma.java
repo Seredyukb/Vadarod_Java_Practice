9ba76f0eaa26b447d5635de6fc9411712ce5e46e
Bogdan <seredyuk.b@gmail.com>
2025-11-19 20:26:15 +0300

    Task_17_18

---
+++ src/Task_17_18/Ferma.java
@@ -0,0 +1,21 @@
+package Task_17_18;
+
+import java.io.Serializable;
+
+public class Ferma implements Serializable {
+    private String name;
+    private int pigCount;
+
+    @Override
+    public String toString() {
+        return "Ferma{" +
+                "name='" + name + '\'' +
+                ", pigCount=" + pigCount +
+                '}';
+    }
+
+    public Ferma(String name, int pigCount) {
+        this.name = name;
+        this.pigCount = pigCount;
+    }
+}
